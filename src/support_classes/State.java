eabf925926d2a39300a61edba5a5afba8c37ce2a
EfaOsk <42963551+EfaOsk@users.noreply.github.com>
2021-02-14 15:33:15 +0000

    Implimented the simple heuristic

--- src/support_classes/State.java
+++ src/support_classes/State.java
@@ -14,6 +14,7 @@ public class State implements Cloneable {
 
     public HashSet<Pawn> whitePawns, blackPawns, pawns;
     public Map<Integer, HashMap<Integer, Pawn>>whiteMap, blackMap;//map.get(x).get(y) -> pawn.
+    public boolean whites_turn; // How's turn is it?
     
     public State() {
         whitePawns = new HashSet<Pawn>();
@@ -21,6 +22,7 @@ public class State implements Cloneable {
         blackPawns = new HashSet<Pawn>();
         blackMap = new HashMap<Integer, HashMap<Integer, Pawn>>();
         pawns = new HashSet<Pawn>();
+
     }
 
 
