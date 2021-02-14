6c02ed47e19b6daba5bd099f6c8b3c114fa29241
vianey17 <vianey17@ru.is>
2021-02-14 20:12:09 +0000

    Save the environment!

--- src/Environment.java
+++ src/Environment.java
@@ -20,9 +20,6 @@ public class Environment {
         this.currentState = new State(width, height);
 
     }
-    public int getHeigth() {
-        return this.height;
-    }
 
     public State getCurrentState(){
         return this.currentState;
@@ -41,25 +38,24 @@ public class Environment {
         Iterator<Action> iterrer = new Iterator<Action>(){
             Iterator<Pawn> pawns = color?state.whitePawns.iterator():state.blackPawns.iterator();
             
-            Iterator<Action> actions = new Iterator<Action>(){
-                public boolean hasNext(){return false;}
-                public Action next(){return null;}
-            };
+            Iterator<Action> actions = null;
 
             public boolean hasNext()
             {
-                while (!actions.hasNext())
-                {
-                    if (!pawns.hasNext())
-                        return false;
-                    actions = pawns.next().moves.iterator();
-                }
-                return true;
+                return pawns.hasNext() || actions.hasNext();
             }
 
             public Action next()
             {
-                hasNext();
+                System.out.println(actions);
+                if (actions == null){
+                    System.out.println("actions null");
+                    actions = pawns.next().moves.iterator();}
+                if (!actions.hasNext()){
+                    System.out.println("actions don't have next");
+                    actions = pawns.next().moves.iterator();
+                }
+                System.out.println(actions);
                 return actions.next();
             }
         };
@@ -77,15 +73,63 @@ public class Environment {
         return a copy of the given state 
     */
     public State getNextState (State state, Action lastMove) {
-        return state;
-        /*State new_state = (State)state.clone();
-        boolean color = state.whiteMap.containsKey(lastMove.x1);
-        if (color)
-            color = color && state.whiteMap.get(lastMove.x1).containsKey(lastMove.y1);
-        if (!color)//it's not white, let's check if it's black (just to be safe)
+        // executes action on clone state (gives what if)
+        // given state and an Action x1, y1, x2, y2
+        /*  3 things need to happen:
+            *Any pawn in (x2,y2) needs to be removed from all storage (it got eaten)
+            *whatever pawn that was in (x1,y1) must be moved to (x2,y2)
+            *every pawn in affected coordinates must have their actions updated
+        */
+        State newState = (State)state.clone();
+        boolean color = state.whiteMap.containsKey(lastMove.x1);  // lastMove type Action
+
+        if (color){ // Is the player white? ++
+            // next, we verify pawn in selected square belongs to current player
+            color = color && state.whiteMap.get(lastMove.x1).containsKey(lastMove.y1); // nested dictionaries, riiiiight
+            
+            // check move legality, for white: current move >= lastmove (but xy1 != xy2 because that's not a move at all)
+            Pawn currentPawn = (Pawn)state.whiteMap.get(lastMove.x1).get(lastMove.y1);  // getting current pawn
+
+            // if move is forward, invalid if any pawn in the way.
+            if ((currentPawn.x == lastMove.x2)&&(currentPawn.y + 1 == lastMove.y2)){
+                Pawn otherPawn = (Pawn)state.blackMap.get(lastMove.x2).get(lastMove.y2);
+                Pawn whitePawn = (Pawn)state.whiteMap.get(lastMove.x2).get(lastMove.y2);
+                if (otherPawn == null || whitePawn == null){
+                    // No pawns at target location, move is acceptable
+                    System.out.println("move is acceptable");
+                    // we update the state to replace currentPawn with newPawn in state.whiteMap
+                }
+                // out here, otherPawn/whitePawn != null, move is invalid.
+            }
+            // else if move is diagonal RIGHT, require a BLACK pawn
+            else if ((currentPawn.x + 1 == lastMove.x2) && (currentPawn.y + 1 == lastMove.y2)){
+                System.out.println("diagonal RIGHT");
+                Pawn otherPawn = (Pawn)state.blackMap.get(lastMove.x2).get(lastMove.y2);
+                System.out.println("Is there a pawn? " + otherPawn);
+            }
+            // else if move is diagonal LEFT, require a BLACK pawn
+            else if ((currentPawn.x == lastMove.x2 - 1) && (currentPawn.y == lastMove.y2 -1)){
+                System.out.println("diagonal RIGHT");
+                Pawn otherPawn = (Pawn)state.blackMap.get(lastMove.x2).get(lastMove.y2);
+                System.out.println("Is there a pawn? " + otherPawn);
+            }
+
+            
+    }       
+
+        /*
+       
+
+
+        if (color) // white player
+            color = color && state.whiteMap.get(lastMove.x1).containsKey(lastMove.y1); // nested dictionaries, riiiiight
+            system.out.println(color);
+            // we want to know if the pawn in this square belongs to the current player.
+
+        else if (!color) //it's not white, let's check if it's black (just to be safe)
         {
             if (!state.blackMap.containsKey(lastMove.x1)){
-                //throw Exception //was not a legal move.
+                //throw Exception //was not a legal move.  
             }
             if (!state.blackMap.get(lastMove.x1).containsKey(lastMove.y1)){
                 //throw Exception //was not a legal move.
@@ -98,17 +142,23 @@ public class Environment {
         if(new_state.blackPawns.contains(new_pawn)){
             new_state.blackPawns.remove(new_pawn);
         }*/
+        return newState;
     }
     public static void main(String[] args){
-        var env = new Environment(5,5);
-        System.out.println(env.currentState);
+        var env = new Environment(4,4);
+        //System.out.println(env.currentState);
+        /*
         Iterator<Action> actions = env.legalMoves(env.currentState,true);
         while (actions.hasNext())
         {
             Action action = actions.next();
             System.out.println(action);
-        }
+        }*/
+        // TESTING FUNCTION getNextState(state, action);
+        Action myAction = new Action(1,2,1,3); // should eat pawn
+        State nextState = env.getNextState(env.currentState, myAction);
+
+        //System.out.println(nextState);
+
     }
 }
-
-
