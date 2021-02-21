cb1b92a1e1ec02836a0c6f31b87298675564767f
vianey17 <vianey17@ru.is>
2021-02-21 14:49:52 +0000

    Fixed table

--- src/TransposeTable.java
+++ src/TransposeTable.java
@@ -13,9 +13,8 @@ public class TransposeTable {
     public void storeState(State state){
         // Hashes two halves of a state and stores it as
         // (k,v) in HashMap bucket.
-        int key = state.blackMap.hashCode();
-
-        int value = state.whiteMap.hashCode();
+        int key = state.blackMap.keySet().hashCode();
+        int value = state.whiteMap.keySet().hashCode();
 
         statesBucket.put(key, value);
 
@@ -24,24 +23,26 @@ public class TransposeTable {
 
     public boolean containsState(State state){
         // returns true/false if state is in collection
-        int key = state.blackMap.hashCode();
-        int value = state.whiteMap.hashCode();
+        int key = state.blackMap.keySet().hashCode();
+        int value = state.whiteMap.keySet().hashCode();
         if (statesBucket.get(key) == null) return false;  // key not found
         if (statesBucket.get(key) == value) return true;  // k/v pair matches
-        
+  
         // will have to account for collisions later
         System.out.println("Collision!");
         return false;  // key found, but value does not match. Collision?
     }
 
     public static void main(String[] args){
-        
+
         State newState = new State(5,5);
-        State otherState = new State(4,4);
+        State copyState = new State(5,5);
         TransposeTable stateTable = new TransposeTable();
-        stateTable.storeState(newState);
-        System.out.println(stateTable.containsState(newState));  // must return true
-        System.out.println(stateTable.containsState(otherState));  // must return false
+        //State copyState = newState.clone();
+        stateTable.storeState(copyState);
+        //stateTable.storeState(newState);
+        System.out.println("Contains state: " + stateTable.containsState(newState));  // must return true
+        //System.out.println(stateTable.containsState(copyState));  // must return false
         // I'm just making this comment to step on Siggi's lawn.
 
     }
